b257d42afe148ca1d6fc17642a833b473eb2ade6
rifandani <ipandani2505@gmail.com>
2023-08-07 21:06:58 +0700

    chore: add context

--- reactivity.js
+++ reactivity.js
@@ -1,23 +1,18 @@
-// ----------------------------------------------------------------
-// Vue 3 Reactivity
-// Marc Backes (@themarcba)
-// ----------------------------------------------------------------
+const product = { price: 15, quantity: 2 };
 
-const product = { price: 15, quantity: 2 }
+let total = product.price * product.quantity;
 
-let total = product.price * product.quantity
+console.log({ total });
 
-console.log({ total })
+product.quantity = 3;
+console.log({ total }); // Does not recalculate
 
-product.quantity = 3
-console.log({ total }) // Does not recalculate
-
-product.price = 12
-console.log({ total }) // Does not recalculate
+product.price = 12;
+console.log({ total }); // Does not recalculate
 
 // executing total = product.price * product.quantity
 // every time something changes would be tedious 😣
 
-product.price = 20
-total = product.price * product.quantity
-console.log({ total }) // Recalculate due to line 22
+product.price = 20;
+total = product.price * product.quantity;
+console.log({ total }); // Recalculate due to line 22
