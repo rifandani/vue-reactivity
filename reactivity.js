db31fde729321ba45f08ee3ff5edd05a51fdffc4
rifandani <ipandani2505@gmail.com>
2023-08-13 22:57:47 +0700

    chore: add context and comment

--- reactivity.js
+++ reactivity.js
@@ -1,78 +1,75 @@
-// ----------------------------------------------------------------
-// Vue 3 Reactivity
-// Marc Backes (@themarcba)
-// ----------------------------------------------------------------
-
-let activeEffect = null
-let targetMap = new WeakMap()
+let activeEffect = null;
+let targetMap = new WeakMap();
 
 // Register an effect
 function track(target, key) {
-    console.log('🔎 track', key)
-    // Get depsMap from targetMap
-    let depsMap = targetMap.get(target)
-    if (!depsMap) {
-        // new depsMap if it doesn't exist yet
-        depsMap = new Map()
-        targetMap.set(target, depsMap)
-    }
+  console.log('🔎 track', key);
 
-    // Get dep from depsMap
-    let dep = depsMap.get(key)
-    if (!dep) {
-        // new dep if it doesn't exist yet
-        dep = new Set()
-        depsMap.set(key, dep)
-    }
+  // Get depsMap from targetMap
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    // new depsMap if it doesn't exist yet
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
 
-    // Add effect
-    if (activeEffect) dep.add(activeEffect)
+  // Get dep from depsMap
+  let dep = depsMap.get(key);
+  if (!dep) {
+    // new dep if it doesn't exist yet
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  // Add effect
+  if (activeEffect) dep.add(activeEffect);
 }
 
 // Execute all registered effects for the target/key combination
 function trigger(target, key) {
-    console.log('💥 trigger', key)
-    // Get depsMap from targetMap
-    let depsMap = targetMap.get(target)
-    if (!depsMap) {
-        // If there is no depsMap, no need to resume
-        return
-    }
+  console.log('💥 trigger', key);
 
-    // Get dep from depsMap
-    let dep = depsMap.get(key)
-    if (!dep) {
-        // If there is no dep, no need to resume
-        return
-    }
+  // Get depsMap from targetMap
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    // If there is no depsMap, no need to resume
+    return;
+  }
 
-    // Execute all effects
-    dep.forEach(effect => effect())
+  // Get dep from depsMap
+  let dep = depsMap.get(key);
+  if (!dep) {
+    // If there is no dep, no need to resume
+    return;
+  }
+
+  // Execute all effects
+  dep.forEach((effect) => effect());
 }
 
 // Makes an object "reactive". Changes will be triggered, once the property is tracked
 function reactive(target) {
-    const handler = {
-        // Intercept getter
-        get(target, key, receiver) {
-            const result = Reflect.get(target, key, receiver)
-            track(target, key) //track changes for the key in the target
-            return result
-        },
-        // Intercept setter
-        set(target, key, value, receiver) {
-            const result = Reflect.set(target, key, value, receiver)
-            trigger(target, key) // trigger a change in the target
-            return result
-        },
-    }
-    return new Proxy(target, handler)
+  const handler = {
+    // Intercept getter
+    get(target, key, receiver) {
+      const result = Reflect.get(target, key, receiver);
+      track(target, key); //track changes for the key in the target
+      return result;
+    },
+    // Intercept setter
+    set(target, key, value, receiver) {
+      const result = Reflect.set(target, key, value, receiver);
+      trigger(target, key); // trigger a change in the target
+      return result;
+    },
+  };
+  return new Proxy(target, handler);
 }
 
 // Watcher
 function effect(fn) {
-    activeEffect = fn
-    // Only execute when there is an activeEffect
-    if (activeEffect) activeEffect()
-    activeEffect = null
+  activeEffect = fn;
+  // Only execute when there is an activeEffect
+  if (activeEffect) activeEffect();
+  activeEffect = null;
 }
