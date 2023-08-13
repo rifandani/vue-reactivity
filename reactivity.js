a4807fef59d817b1954d1fb884afb5407f733d73
rifandani <ipandani2505@gmail.com>
2023-08-13 19:44:20 +0700

    chore: add context

--- reactivity.js
+++ reactivity.js
@@ -1,58 +1,57 @@
-// ----------------------------------------------------------------
-// Vue 3 Reactivity
-// Marc Backes (@themarcba)
-// ----------------------------------------------------------------
+/**
+ * Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object.
+ * A proxy controls access to the original object, allowing you to perform something either before or after the request gets through to the original object.
+ */
 
-const product = { price: 15, quantity: 2 }
+const product = { price: 15, quantity: 2 }; // state
 
-let total = 0
+let total = 0;
+let deps = new Set(); // list of effects will be saved here
 
-const calculateTotal = () => {
-    total = product.price * product.quantity
+function calculateTotal() {
+  total = product.price * product.quantity;
 }
-
-let dep = new Set() // list of effects will be saved here
 
 // Register an effect
 function track(target, key) {
-    // target and key are not used yet.
-    // they will come into play in stage 5
+  // target and key are not used yet.
+  // they will come into play in stage 5
 
-    // Add effect
-    dep.add(calculateTotal)
+  // Add effect
+  deps.add(calculateTotal);
 }
 
 // Execute all registered effects for the target/key combination
 function trigger(target, key) {
-    // target and key are not used yet.
-    // they will come into play in stage 5
+  // target and key are not used yet.
+  // they will come into play in stage 5
 
-    // Execute all effects
-    dep.forEach(effect => effect())
+  // Execute all effects
+  deps.forEach((effect) => effect());
 }
 
 const proxy = new Proxy(product, {
-    // Intercept getter
-    get(target, key, receiver) {
-        console.log('get', key, 'from', target)
-        const result = Reflect.get(target, key, receiver)
-        track(target, key) //track changes for the key in the target
-        return result
-    },
-    // Intercept setter
-    set(target, key, value, receiver) {
-        console.log('set', key, '=>', value)
-        const result = Reflect.set(target, key, value, receiver)
-        trigger(target, key) // trigger a change in the target
-        return result
-    },
-})
+  // Intercept getter
+  get(target, key, receiver) {
+    console.log('get', key, 'from', target);
+    const result = Reflect.get(target, key, receiver);
+    track(target, key); // track changes for the key in the target
+    return result;
+  },
+  // Intercept setter
+  set(target, key, value, receiver) {
+    console.log('set', key, '=>', value);
+    const result = Reflect.set(target, key, value, receiver);
+    trigger(target, key); // trigger a change in the target
+    return result;
+  },
+});
 
-console.log(proxy.price)
-proxy.price = 25
-console.log(proxy.price)
-proxy.quantity = 5
-console.log(proxy.quantity)
+console.log(proxy.price);
+proxy.price = 25;
+console.log(proxy.price);
+proxy.quantity = 5;
+console.log(proxy.quantity);
 
 // Results in:
 // get price from {price: 15, quantity: 2}
